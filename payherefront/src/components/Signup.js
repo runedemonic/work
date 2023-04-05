977f8fb4999d106b10dbe4c519d43a1b8c6733fa
runedemonic <runedemonic@naver.com>
2023-04-05 18:10:23 +0900

    Signup

---
+++ payherefront/src/components/Signup.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Signup = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+
+    try {
+      const response = await axios.post('http://localhost:8000/signup/', {
+        username: username,
+        password: password,
+      });
+
+      if (response.status === 201) {
+        alert('User created successfully');
+      }
+    } catch (error) {
+      setError('Username and password are required');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Signup</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default Signup;
